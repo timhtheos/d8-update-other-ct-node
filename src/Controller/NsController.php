29a854e88c85233081640d928561cdc82c9d9a55
timhtheos <me@timothyae.com>
2019-02-21 01:51:09 +0800

    Add method updateDevice into NsController.

--- src/Controller/NsController.php
+++ src/Controller/NsController.php
@@ -11,5 +11,32 @@ use Drupal\Core\Controller\ControllerBase;
  * Class NsController.
  */
 class NsController extends ControllerBase {
+  
+  /**
+   * Update CT device.
+   *
+   * @param object $node
+   *   Node entity object.
+   *
+   * @return null
+   *   Nothing is returned.
+   */
+  function updateDevice($node) {
+    // We get the node id referenced in field_device in CT worklog.
+    // Not tested. Please check, xD.
+    $nid_device = $node->get('field_device')
+      ->first() // If multiple. I'm not sure if this is needed or ok if stays as-is if the cardinality is 1.
+      ->get('entity')
+      ->getTarget()
+      ->getValue()
+      ->id();
 
+    // Load CT device.
+    $node_device = \Drupal::entityTypeManager()->getStorage('node')->load($nid_device);
+
+    $node_device->set('field_next_step', $node->get('title')->value);
+
+    // Save/update node in CT device.
+    $node_device->save();
+  }
 }
