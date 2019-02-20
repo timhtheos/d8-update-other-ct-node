c009b23aff0f7b86747172f5778b8753cfbd1cff
timhtheos <me@timothyae.com>
2019-02-21 01:42:48 +0800

    Add controller NsController.

---
+++ src/Controller/NsController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Next service awesome controller.
+ */
+
+namespace Drupal\next_service\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class NsController.
+ */
+class NsController extends ControllerBase {
+
+}
